c5c9428b914e0c197c32006cde431e46077926ae
AlexFolland <lexlexlex@gmail.com>
2018-08-04 14:55:25 -0700

    intitial commit

---
+++ ChatInfoStuff.lua
@@ -0,0 +1,4 @@
+if GetRegisteredAddonMessagePrefixes == nil then GetRegisteredAddonMessagePrefixes = C_ChatInfo.GetRegisteredAddonMessagePrefixes end
+if IsAddonMessagePrefixRegistered == nil then IsAddonMessagePrefixRegistered = C_ChatInfo.IsAddonMessagePrefixRegistered end
+if RegisterAddonMessagePrefix == nil then RegisterAddonMessagePrefix = C_ChatInfo.RegisterAddonMessagePrefix end
+if SendAddonMessage == nil then SendAddonMessage = C_ChatInfo.SendAddonMessage end
